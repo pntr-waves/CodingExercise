8f3b0db2ddab0b7cc560fc3a986ce5addd6f549d
pntr-waves <phannhattruong2505@gmail.com>
2022-06-02 21:01:12 +0700

    init manage book

---
+++ 30-5-2022 to 5-6-2022/Exercise1000/src/truong/exercise1000/managebook/data/TestDataBuilder.java
@@ -0,0 +1,15 @@
+package truong.exercise1000.managebook.data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import truong.exercise1000.managebook.model.Book;
+
+public class TestDataBuilder {
+	public static List<Book> getListBook() {
+		List<Book> bookList = new ArrayList<>();
+		
+		
+		return null;
+	}
+}
