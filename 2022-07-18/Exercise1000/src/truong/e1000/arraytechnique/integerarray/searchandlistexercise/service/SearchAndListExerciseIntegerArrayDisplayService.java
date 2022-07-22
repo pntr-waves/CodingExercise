28c5beb6e58f4c94b5fd126184abf15ad6c8d8f3
truongphann <truong.phan@evolus.vn>
2022-07-22 10:17:22 +0700

    implement search and list exercise

---
+++ 2022-07-18/Exercise1000/src/truong/e1000/arraytechnique/integerarray/searchandlistexercise/service/SearchAndListExerciseIntegerArrayDisplayService.java
@@ -0,0 +1,9 @@
+package truong.e1000.arraytechnique.integerarray.searchandlistexercise.service;
+
+public class SearchAndListExerciseIntegerArrayDisplayService {
+    public void printAll (int[] array) {
+        for (int i : array) {
+            System.out.println(i);
+        }
+    }
+}
